14294b90ae5cba93ca27cd0037e3d556aa8fbc51
unknown <sl75529787@idat.edu.pe>
2023-08-05 07:01:09 -0500

    Mostrar la vista detalleproducto al usuario, pero sin la información del producto

--- src/main/java/idat/Proyecto/controller/HomeController.java
+++ src/main/java/idat/Proyecto/controller/HomeController.java
@@ -1,9 +1,12 @@
 package idat.Proyecto.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import idat.Proyecto.service.ProductoService;
@@ -11,6 +14,9 @@ import idat.Proyecto.service.ProductoService;
 @Controller
 @RequestMapping("/")//Apunte a la raíz
 public class HomeController {
+	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
+	
 
 	@Autowired
 	private ProductoService prs;
@@ -21,5 +27,12 @@ public class HomeController {
 		
 		return "usuario/home";
 		
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("Id producto enviado como parámtero{}", id);
+		
+		return "usuario/productohome";
 	}
 }
