d4701325663ce2f4764ec8e31c66283571b8de29
unknown <sl75529787@idat.edu.pe>
2023-08-05 07:34:12 -0500

    Mostrar la vista carrito cuando el usuario le da clic a agregar a carrito, pero no con la información del producto

--- src/main/java/idat/Proyecto/controller/HomeController.java
+++ src/main/java/idat/Proyecto/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import idat.Proyecto.entity.Producto;
@@ -43,5 +44,12 @@ public class HomeController {
 		
 		model.addAttribute("producto", producto);
 		return "usuario/productohome";
+	}
+	
+	@PostMapping("/cart")
+	public String addproductoCarrito() {
+		
+		
+		return "usuario/carrito";
 	}
 }
