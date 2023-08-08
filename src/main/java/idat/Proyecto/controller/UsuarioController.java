e32cc72f10afccc2fb360541a470287c45299299
unknown <sl75529787@idat.edu.pe>
2023-08-08 12:29:31 -0500

    Mostrar la vista compras para el usuario

--- src/main/java/idat/Proyecto/controller/UsuarioController.java
+++ src/main/java/idat/Proyecto/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -73,6 +74,14 @@ public class UsuarioController {
 		}
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/compras")
+	public String compraUsuario(Model model, HttpSession session) {
+		
+		//Session
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		return "usuario/compras";
 	}
 	
 	
