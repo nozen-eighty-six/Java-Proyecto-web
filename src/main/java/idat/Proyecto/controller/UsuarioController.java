5909d41e0c222ec5456a603ba1d6e7c1cad6844e
unknown <sl75529787@idat.edu.pe>
2023-08-09 11:39:38 -0500

    Funcionalidad de cerrar sesión del usuario

--- src/main/java/idat/Proyecto/controller/UsuarioController.java
+++ src/main/java/idat/Proyecto/controller/UsuarioController.java
@@ -112,7 +112,13 @@ public class UsuarioController {
 		
 		return "usuario/detallecompra";
 	}
-	
+	@GetMapping("/cerrar")
+	public String cerrarSesion(HttpSession session) {
+		
+		session.removeAttribute("idusuario");
+		
+		return "redirect:/";
+	}
 	
 
 }
